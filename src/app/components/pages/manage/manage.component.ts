52ec47bda81c249897f13f1c28b52c87d6686b43
Noel RIGNON <noel.rignon@fjnr.ca>
2018-09-08 12:13:35 -0400

    integration of question section

--- src/app/components/pages/manage/manage.component.ts
+++ src/app/components/pages/manage/manage.component.ts
@@ -9,13 +9,97 @@ export class ManageComponent implements OnInit {
 
   step: number = null;
 
+  questions = [
+    {
+      id: 1,
+      titleGodson: 'Quel niveau d\'etude allez vous commencer ?',
+      titleGodfather: 'A quel niveau d\'etude etes-vous ?',
+      answers: [
+        {
+          id: 1,
+          title: 'Baccalaureat'
+        },
+        {
+          id: 2,
+          title: 'Maitrise'
+        },
+        {
+          id: 3,
+          title: 'Doctorat'
+        }
+      ]
+    },
+    {
+      id: 2,
+      titleGodson: 'Lorem ipsum son',
+      titleGodfather: 'Lorem ipsum father',
+      answers: [
+        {
+          id: 1,
+          title: 'A'
+        },
+        {
+          id: 2,
+          title: 'B'
+        }
+      ]
+    }
+  ];
+
+  selectedQuestion: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.selectedQuestion = this.questions[0].id;
   }
 
   changeStep(index: number) {
     this.step = index;
   }
 
+  addNewQuestion() {
+    this.questions.push(
+      {
+        id: this.questions.length + 1,
+        titleGodson: '',
+        titleGodfather: '',
+        answers: [
+          {
+            id: 1,
+            title: ''
+          },
+          {
+            id: 2,
+            title: ''
+          }
+        ]
+      },
+    );
+  }
+
+  addNewAnswer(questionId: number) {
+    for (const question of this.questions) {
+      if (question.id === questionId) {
+        question.answers.push(
+          {
+            id: question.answers.length + 1,
+            title: ''
+          }
+        );
+      }
+    }
+  }
+
+  changeSelectedQuestion(questionId: number) {
+    this.selectedQuestion = questionId;
+  }
+
+  getSelectedQuestion() {
+    for (const question of this.questions) {
+      if (question.id === this.selectedQuestion) {
+        return question;
+      }
+    }
+  }
 }
